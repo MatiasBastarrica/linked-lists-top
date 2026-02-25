9f973f3edee2c4b7e165baf73276d00c77ad25b2
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-25 11:51:07 -0300

    Write the "size" function

--- linked-lists.js
+++ linked-lists.js
@@ -16,4 +16,23 @@ class LinkedList {
     const nextNode = this.head;
     this.head = new Node(value, nextNode);
   }
+
+  size() {
+    let total = 0;
+    if (!this.head) {
+      return 0;
+    } else if (!this.head.nextNode) {
+      return 1;
+    }
+
+    for (
+      let currentNode = this.head;
+      this.head.nextNode;
+      currentNode = this.head.nextNode
+    ) {
+      total += 1;
+    }
+
+    return size;
+  }
 }
